614b24258aed616da993b3c47af49aa6d59c0813
adolfo <aldomc99@gmail.com>
2025-04-08 10:00:02 -0600

    Se agrega html, css y js (ej. Estructura de datos)

---
+++ modulo_04/clase02_20_2025/estructura_de_datos/lista_compras.js
@@ -0,0 +1,46 @@
+const productos = [];
+function agregarProducto() {
+    const input = document.getElementById("producto");
+    const nombreProducto = input.value.trim(); 
+    if (nombreProducto === "") {
+        alert("Ingresa el nombre de algún producto.");
+        return;
+    }
+    if (productos.includes(nombreProducto)) {
+        alert("Este producto ya está en la lista.");
+        return;
+    }
+    productos.push(nombreProducto);
+    actualizarLista();
+    input.value = "";
+}
+
+function eliminarProducto(nombreProducto) {
+    const index = productos.indexOf(nombreProducto);
+    if (index !== -1) { 
+        productos.splice(index, 1); 
+        actualizarLista(); 
+    }
+}
+
+function vaciarLista() {
+    productos.length = 0;
+    actualizarLista();
+}
+
+function actualizarLista() {
+    const lista = document.getElementById("lista_productos"); 
+    lista.innerHTML = ""; 
+    productos.forEach((producto, index) => {
+        const li = document.createElement("li"); 
+        const productoAgregado = document.createTextNode(`${index+1}. ${producto} `);
+        li.appendChild(productoAgregado);
+        
+        const btnEliminar = document.createElement("button");
+        btnEliminar.textContent = "Eliminar producto"; 
+        btnEliminar.classList.add("btn-delete"); 
+        btnEliminar.onclick = () => eliminarProducto(producto); 
+        li.appendChild(btnEliminar); 
+        lista.appendChild(li); 
+    });
+}
---
+++ modulo_04/clase02_20_2025/gastos/main.js
@@ -0,0 +1,7 @@
+const gastos = [];
+
+function agregarGasto() {
+    // 🚀 Completar: Obtener valores del input y validar que sean correctos.
+    // 🚀 Completar: Agregar un objeto con descripción y monto al array gastos.
+    
+}
