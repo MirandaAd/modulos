44f493fa1a4ea863741e9886ac93deab4040cdf7
adolfo <aldomc99@gmail.com>
2025-04-12 15:01:02 -0600

    Se agrega exp. de ej. divide & conquer

--- modulo_04/clase07_08_04_2025/Divide&Conquer/divideConquer.js
+++ modulo_04/clase07_08_04_2025/Divide&Conquer/divideConquer.js
@@ -1,13 +1,14 @@
 function findMax(arr){
+    // Caso base
     if(arr.length === 1){
-        return arr[0];
+        return arr[0]; //Devuelve el primer elemento
     }
-    const mid = Math.floor(arr.length / 2);
-    const left = arr.slice(0, mid);
-    const right = arr.slice(mid);
-    const maxLeft = findMax(left);
-    const maxRight = findMax(right);
-    return Math.max(maxLeft, maxRight);
+    const mid = Math.floor(arr.length / 2); // Divide el arreglo a la mitad
+    const left = arr.slice(0, mid); //Mitad de la izquierda
+    const right = arr.slice(mid); //Mitad de la derecha
+    const maxLeft = findMax(left); // Recursividad para la mitad izquierda
+    const maxRight = findMax(right); // Recursividad para la mitad derecha
+    return Math.max(maxLeft, maxRight); //Devuelve el máximo de las dos partes
 }
 
 const numeros = [3, 8, 2, 10, 5, 7];
