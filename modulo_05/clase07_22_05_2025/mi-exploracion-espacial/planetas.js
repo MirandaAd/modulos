e89266dfcdfddda0417075b99f16d95c4576ca20
adolfo <aldomc99@gmail.com>
2025-05-31 00:34:00 -0600

    Ej. Planetas - intro a node.js & npm

---
+++ modulo_05/clase07_22_05_2025/mi-exploracion-espacial/planetas.js
@@ -0,0 +1,54 @@
+const planetas = [
+    {
+        nombre: "Próxima Centauri b",
+        descripcion: "Un exoplaneta rocoso en la zona habitable de su estrella.",
+        descubiertoEn: "2016"
+    },
+    {
+        nombre: "Titán",
+        descripcion: "La luna más grande de Saturno, con lagos de metano.",
+        descubiertoEn: "1655"
+    },
+    {
+        nombre: "Mercurio",
+        descripcion: "El planeta más cercano al Sol.",
+        descubiertoEn: "1610"
+    },
+    {
+        nombre: "Venus",
+        descripcion: "El planeta con atmosfera densa y tóxica.",
+        descubiertoEn: "Desde la antiguidad"
+    },
+    {
+        nombre: "Tierra",
+        descripcion: "El planeta mas habitable del Sistema Solar.",
+        descubiertoEn: "Desde la antiguidad"
+    },
+    {
+        nombre: "Marte",
+        descripcion: "Planeta rocoso y con una atmósfera delgada.",
+        descubiertoEn: "Desde la antiguidad"
+    },
+    {
+        nombre: "Jupiter",
+        descripcion: "El planeta mas grande del Sistema Solar.",
+        descubiertoEn: "Desde la antiguidad"
+    },
+    {
+        nombre: "Saturno",
+        descripcion: "Planeta gaseosos con un sistema de anillos.",
+        descubiertoEn: "Desde la antiguidad"
+    },
+    {
+        nombre: "Urano",
+        descripcion: "William Herschel descubrió este planeta.",
+        descubiertoEn: "1781"
+    },
+    {
+        nombre: "Neptuno",
+        descripcion: "Planeta descubierto por Urbaine Le Verrier.",
+        descubiertoEn: "1846"
+    }
+];
+
+module.exports = planetas;
